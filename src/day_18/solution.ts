41147a7b8fc2df4b31b8a85f6ea0d53b8cb6d83e
Fadi George <fadii925@gmail.com>
2022-12-28 16:57:57 -0800

    add day 18 part 2

--- src/day_18/solution.ts
+++ src/day_18/solution.ts
@@ -1,13 +1,14 @@
-import { keyToPoint, pointToKey, printObject, readInput } from "../helpers"
+import { keyToPoint, pointToKey, readInput } from "../helpers"
 
-const input = readInput(false)
+const input = readInput(true)
 const grid = new Set<string>()
 
 input.forEach((line) => {
   grid.add(line)
 })
 
-const intersections: Record<string, number> = {}
+const pocketSet = new Set<string>()
+
 const getNeighbors = (x: number, y: number, z: number) => [
   [x - 1, y, z], // left
   [x + 1, y, z], // right
@@ -17,19 +18,34 @@ const getNeighbors = (x: number, y: number, z: number) => [
   [x, y, z + 1], // back
 ]
 
+let minX = Infinity
+let maxX = -Infinity
+let minY = Infinity
+let maxY = -Infinity
+let minZ = Infinity
+let maxZ = -Infinity
+
 const gridArr = [...grid]
 const total = gridArr.reduce((acc, point) => {
   const [x, y, z] = point.split(",").map(Number)
+
+  minX = Math.min(minX, x)
+  maxX = Math.max(maxX, x)
+  minY = Math.min(minY, y)
+  maxY = Math.max(maxY, y)
+  minZ = Math.min(minZ, z)
+  maxZ = Math.max(maxZ, z)
 
   let surfaceArea = 6
 
   const neighbors = getNeighbors(x, y, z)
   neighbors.forEach(([i, j, k]) => {
     const newPoint = pointToKey(i, j, k)
-    intersections[newPoint] = 1
 
     if (grid.has(newPoint)) {
       surfaceArea--
+    } else {
+      pocketSet.add(newPoint)
     }
   })
 
@@ -40,130 +56,62 @@ const total = gridArr.reduce((acc, point) => {
 logP1(total)
 
 // Part 2
-const neighborsSet = new Set<string>()
+minX--
+maxX++
+minY--
+maxY++
+minZ--
+maxZ++
 
-// count intersections
-gridArr.forEach((point) => {
-  let minX: number | undefined
-  let maxX: number | undefined
-  let minY: number | undefined
-  let maxY: number | undefined
-  let minZ: number | undefined
-  let maxZ: number | undefined
-  const [x, y, z] = keyToPoint(point)
-  gridArr.forEach((point2) => {
-    if (point === point2) return
-    const [x2, y2, z2] = keyToPoint(point2)
-    const diffX = x2 - x
-    const diffY = y2 - y
-    const diffZ = z2 - z
-    if (diffX && diffY === 0 && diffZ === 0) {
-      if (diffX < 0) minX = Math.max(minX || -Infinity, diffX)
-      else maxX = Math.min(maxX || Infinity, diffX)
-    } else if (diffY && diffX === 0 && diffZ === 0) {
-      if (diffY < 0) minY = Math.max(minY || -Infinity, diffY)
-      else maxY = Math.min(maxY || Infinity, diffY)
-    } else if (diffZ && diffX === 0 && diffY === 0) {
-      if (diffZ < 0) minZ = Math.max(minZ || -Infinity, diffZ)
-      else maxZ = Math.min(maxZ || Infinity, diffZ)
-    }
+const queue = [
+  {
+    x: minX,
+    y: minY,
+    z: minZ,
+  },
+]
+const outsideSet = new Set<string>()
+while (queue.length) {
+  const item = queue.shift()
+
+  if (!item) continue
+  const { x, y, z } = item
+
+  const neighbors = getNeighbors(x, y, z)
+  neighbors.forEach(([i, j, k]) => {
+    const newPoint = pointToKey(i, j, k)
+
+    if (i < minX || i > maxX) return
+    if (j < minY || j > maxY) return
+    if (k < minZ || k > maxZ) return
+    if (grid.has(newPoint) || outsideSet.has(newPoint)) return
+
+    outsideSet.add(newPoint)
+    queue.push({ x: i, y: j, z: k })
   })
-  // printObject({ minX, maxX, minY, maxY, minZ, maxZ, point })
+}
 
-  if (maxX) {
-    for (let i = 1; i < maxX; i++) {
-      const pointKey = pointToKey(x + i, y, z)
-      if (!neighborsSet.has(pointKey)) {
-        neighborsSet.add(pointKey)
-      } else {
-        intersections[pointKey] = (intersections[pointKey] || 0) + 1
-      }
-    }
-  }
-  if (minX) {
-    for (let i = -1; i > minX; i--) {
-      const pointKey = pointToKey(x + i, y, z)
-      if (!neighborsSet.has(pointKey)) {
-        neighborsSet.add(pointKey)
-      } else {
-        intersections[pointKey] = (intersections[pointKey] || 0) + 1
-      }
-    }
-  }
-  if (maxY) {
-    for (let i = 1; i < maxY; i++) {
-      const pointKey = pointToKey(x, y + i, z)
-      if (!neighborsSet.has(pointKey)) {
-        neighborsSet.add(pointKey)
-      } else {
-        intersections[pointKey] = (intersections[pointKey] || 0) + 1
-      }
-    }
-  }
-  if (minY) {
-    for (let i = -1; i > minY; i--) {
-      const pointKey = pointToKey(x, y + i, z)
-      if (!neighborsSet.has(pointKey)) {
-        neighborsSet.add(pointKey)
-      } else {
-        intersections[pointKey] = (intersections[pointKey] || 0) + 1
-      }
-    }
-  }
-  if (maxZ) {
-    for (let i = 1; i < maxZ; i++) {
-      const pointKey = pointToKey(x, y, z + i)
-      if (!neighborsSet.has(pointKey)) {
-        neighborsSet.add(pointKey)
-      } else {
-        intersections[pointKey] = (intersections[pointKey] || 0) + 1
-      }
-    }
-  }
-  if (minZ) {
-    for (let i = -1; i > minZ; i--) {
-      const pointKey = pointToKey(x, y, z + i)
-      if (!neighborsSet.has(pointKey)) {
-        neighborsSet.add(pointKey)
-      } else {
-        intersections[pointKey] = (intersections[pointKey] || 0) + 1
-      }
-    }
+let pocketArr = [...pocketSet]
+pocketArr.forEach((point) => {
+  if (outsideSet.has(point)) {
+    pocketSet.delete(point)
   }
 })
 
-const pocketArea = Object.entries(intersections).reduce(
-  (acc, [point, count]) => {
-    if (count === 6) {
-      const [x, y, z] = keyToPoint(point)
-      let area = 0
+pocketArr = [...pocketSet]
+const pocketArea = pocketArr.reduce((acc, point) => {
+  const [x, y, z] = keyToPoint(point)
+  const neighbors = getNeighbors(x, y, z)
 
-      getNeighbors(x, y, z).forEach(([i, j, k]) => {
-        const newPoint = pointToKey(i, j, k)
-        if (grid.has(newPoint)) {
-          area++
-        }
-      })
-
-      return acc + area
+  let area = 0
+  neighbors.forEach(([i, j, k]) => {
+    const newPoint = pointToKey(i, j, k)
+    if (grid.has(newPoint)) {
+      area++
     }
+  })
 
-    return acc
-  },
-  0
-)
+  return acc + area
+}, 0)
 
-console.log(
-  Object.entries(intersections)
-    .sort((p1, p2) => {
-      const [x1, y1, z1] = keyToPoint(p1[0])
-      const [x2, y2, z2] = keyToPoint(p2[0])
-
-      return x1 - x2 || y1 - y2 || z1 - z2
-    })
-    .filter(([point, count]) => count === 5)
-)
 logP2(total - pocketArea)
-
-// 4010 too high
-// 2451 too low
